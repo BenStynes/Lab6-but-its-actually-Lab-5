08daa5a08b02568726735d71752b39f95c6dd9ee
(Student) - Ben Stynes <c00239718@ITCARLOW.IE>
2019-12-03 10:52:49 +0000

    AYYYY

--- LAB6-but-aCTUALLY-LAB5/LAB6-but-aCTUALLY-LAB5/Quaternion.h
+++ LAB6-but-aCTUALLY-LAB5/LAB6-but-aCTUALLY-LAB5/Quaternion.h
@@ -2,149 +2,70 @@
 #include  <cstring>
 #include <iostream>
 #include "vector3.h"
+const float PI = 3.14159265359f;
+
 class Quaternion
 {
-			 float
-				 x, y, z, w;
+public:
+	float x, y, z, w;
+	
+	Quaternion();
+			
+			Quaternion(float _w, float _x, float _y, float _z);
+			
 
-			 Quaternion()
-			{
-				w = 0; x = 0; y = 0; z = 0;
-			}
-			 Quaternion(float _w, float _x, float _y, float _z)
-			{
-				w = _w; x = _x; y = _y; z = _z;
-			}
-
-			 Quaternion(float _w, Vector3 v)
-			{
-				w = _w; x = v.X; y = v.Y; z = v.Z;
-			}
-
-			  std::string ToString()
-			{
-				return "(" + w.ToString("g3") + "," + x.ToString("g3") + "i," + y.ToString("g3") + "j," + z.ToString("g3") + "k)";
-			}
+			 Quaternion(float _w, vector3 v);
+			
+			std::string ToString();
+			
 
 			 float  getModulus(); 
 			
-			 Quaternion Normalise()
-			{
-				float m = w * w + x * x + y * y + z * z;
-				if (m > 0.001)
-				{
-					m = Math.Sqrt(m);
-					return new Quaternion(w / m, x / m, y / m, z / m);
-				}
-				else
-				{
-					return new Quaternion(1, 0, 0, 0);
-				}
-			}
+			 Quaternion Normalise();
+			
+			Quaternion Conjugate();
+			
 
-			 Quaternion Conjugate()
-			{
-				return new Quaternion(w, -x, -y, -z);
-			}
+			void FromAxisAngle(vector3 axis, float angleRadian);
+			
+			Quaternion Multiply(Quaternion _q);
+			
 
-			 void FromAxisAngle(Vector3 axis, float angleRadian)
-			{
-				float m = axis.Length();
-				if (m > 0.0001)
-				{
-					float ca = Math.Cos(angleRadian / 2);
-					float sa = Math.Sin(angleRadian / 2);
-					x = axis.X / m * sa;
-					y = axis.Y / m * sa;
-					z = axis.Z / m * sa;
-					w = ca;
-				}
-				else
-				{
-					w = 1; x = 0; y = 0; z = 0;
-				}
-			}
-
-			 Quaternion Multiply(Quaternion _q)
-			{
-				float nw = _q.w * w - _q.x * x - _q.y * y - _q.z * z;
-				float nx = _q.w * x + _q.x * w + _q.y * z - _q.z * y;
-				float ny = _q.w * y + _q.y * w + _q.z * x - _q.x * z;
-				float nz = _q.w * z + _q.z * w + _q.x * y - _q.y * x;
-				return new Quaternion(nw, nx, ny, nz);
-			}
-
-			 Quaternion Copy()
-			{
-				return new Quaternion(x, y, z, w);
-			}
+			Quaternion Copy();
+		
 			//                  -1
 			// V'=q*V*q     ,
 
-			 Vector3 Rotate(Vector3 pt, int _angle)
-			{
-				Quaternion axis, rotate;
-				axis = this.Normalise();
-				float angleRad = Math.PI / 180 * _angle;
-				rotate = new Quaternion(Math.Cos(angleRad / 2), Math.Sin(angleRad / 2) * axis.x, Math.Sin(angleRad / 2) * axis.y, Math.Sin(angleRad / 2) * axis.z);
-				Quaternion conjugate = rotate.Conjugate();
-				Quaternion qNode = new Quaternion(0, pt.X, pt.Y, pt.Z);
-				qNode = rotate * qNode * conjugate;
-				return new Vector3(qNode.x, qNode.y, qNode.z);
-			}
+			friend vector3 Rotate(vector3 pt, int _angle);
+			
 
 			// Multiplying q1 with q2 is meaning of doing q2 firstly then q1
-			  Quaternion operator +(Quaternion q1, Quaternion q2)
-			{
-				return new Quaternion(q1.w + q2.w, q1.x + q2.x, q1.y + q2.y, q1.z + q2.z);
-			}
-
-			  Quaternion operator -(Quaternion q1)
-			{
-				return new Quaternion(-q1.w, -q1.x, -q1.y, -q1.z);
-			}
-
-			  Quaternion operator -(Quaternion q1, Quaternion q2)
-			{
-				return new Quaternion(q1.w - q2.w, q1.x - q2.x, q1.y - q2.y, q1.z - q2.z);
-			}
+			 Quaternion operator +(Quaternion q2);
 			
 
-				  Quaternion operator *(Quaternion q1, float s)
-			{
-				return new Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
-			}
-			  Quaternion operator *(Quaternion q1, float s)
-			{
-				return new Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
-			}
-			  Quaternion operator *(Quaternion q1, int s)
-			{
-				return new Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
-			}
-			  Quaternion operator *(float s, Quaternion q1)
-			{
-				return new Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
-			}
-			  Quaternion operator *(float s, Quaternion q1)
-			{
-				return new Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
-			}
-			  Quaternion operator *(int s, Quaternion q1)
-			{
-				return new Quaternion(q1.w * s, q1.x * s, q1.y * s, q1.z * s);
-			}
+			Quaternion operator -(Quaternion q1);
 			
 
-				  Quaternion operator *(Quaternion q1, Quaternion q2)
-			{
-				float nw = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
-				float nx = q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y;
-				float ny = q1.w * q2.y + q1.y * q2.w + q1.z * q2.x - q1.x * q2.z;
-				float nz = q1.w * q2.z + q1.z * q2.w + q1.x * q2.y - q1.y * q2.x;
-				return new Quaternion(nw, nx, ny, nz);
-			}
+			
+			
+			
+
+			Quaternion operator *(float s);
+			
+			friend  Quaternion operator *(Quaternion q1, float s);
+			
+			Quaternion operator *(int s);
+			
+			friend 	  Quaternion operator *(float s, Quaternion q1);
+			
+			friend  Quaternion operator *(float s, Quaternion q1);
+			
+			friend  Quaternion operator *(int s, Quaternion q1);
 		
+			
+
+			friend  Quaternion operator *(Quaternion q1, Quaternion q2);
+			
 	
 
 };
--- LAB6-but-aCTUALLY-LAB5/LAB6-but-aCTUALLY-LAB5/main.cpp
+++ LAB6-but-aCTUALLY-LAB5/LAB6-but-aCTUALLY-LAB5/main.cpp
@@ -1,7 +1,125 @@
 #include <iostream>
+#include "Quaternion.h"
+#include "Matrix3.h"
+#include "Vector2f.h"
+#include "vector3.h"
+#include "main.h"
 
 int main()
 {
+	vector3 testVect3;
+	vector3 testVect31;
+	Matrix3 testMat3;
+	Vector2f testvect2f;
+	Vector2f testvect2f1;
+	Quaternion testquat;
+	testvect2f.setX(1);
+	testvect2f.setY(2);
+
+	std::cout << "Test for Vector2f" << std::endl;
+	testvect2f.Normalise();
+	std::cout << testvect2f.ToString();
+	std::cout << std::endl;
+	std::cout << "Length:" << testvect2f.Length() << std::endl;
+	
+	std::cout << "Length Squared:" << testvect2f.LengthSquared() << std::endl;
+	testvect2f.setX(1);
+	testvect2f.setY(2);
+
+	float n = testvect2f * Vector2f{2, 2};
+	std::cout << n;
+	std::cout << std::endl;
+
+	testvect2f.setX(1);
+	testvect2f.setY(2);
+
+	Vector2f c = testvect2f + Vector2f{ 2,2 };
+	std::cout << c.ToString();
+	std::cout << std::endl;
+	testvect2f.setX(1);
+	testvect2f.setY(2);
+	Vector2f a = testvect2f  - Vector2f{ 2,2 };
+	std::cout << a.ToString();
+	
+	std::cout << std::endl;
+	Vector2f b = -testvect2f;
+	std::cout << b.ToString();
+	std::cout << std::endl;
+	testvect2f.setX(1);
+	testvect2f.setY(2);
+	Vector2f reee = testvect2f * 2;
+	std::cout << reee.ToString();
+	std::cout << std::endl;
+	Vector2f ree =testvect2f * 2.0;
+	std::cout << ree.ToString();
+	std::cout << std::endl;
+	Vector2f re = testvect2f * 2.0f;
+	std::cout << re.ToString();
+	std::cout << std::endl; 
+
+	testVect31.setX(4);
+	testVect31.setY(5);
+	testVect31.setZ(6);
+	
+	testVect3.setX(1);
+	testVect3.setY(2);
+	testVect3.setZ(3);
+	testVect3.Normalise();
+	std::cout << "Test for Vector3f" << std::endl;
+	std::cout << testVect3.ToString();
+	std::cout << std::endl;
+
+
+	testVect3.setX(1);
+	testVect3.setY(2);
+	testVect3.setZ(3);
+	
+	std::cout << "Length:" << testVect3.Length() << std::endl;
+	std::cout << "Length Squared:" << testVect3.LengthSquared() << std::endl;
+
+
+	testVect3.setX(1);
+	testVect3.setY(2);
+	testVect3.setZ(3);
+
+	
+	float x = testVect3 * vector3{ 2, 2,2 };
+	std::cout  << n;
+	std::cout << std::endl;
+
+	testVect3.setX(1);
+	testVect3.setY(2);
+	testVect3.setZ(3);
+
+	vector3 d = testVect3 + vector3{ 2,2,2 };
+	std::cout << d.ToString();
+	std::cout << std::endl;
+	testVect3.setX(1);
+	testVect3.setY(2);
+	testVect3.setZ(3);
+	vector3 f = testVect3 - vector3{ 2,2,2 };
+	std::cout << f.ToString();
+
+	std::cout << std::endl;
+	vector3 bj= -testVect3;
+	std::cout << bj.ToString();
+	std::cout << std::endl;
+	testVect3.setX(1);
+	testVect3.setY(2);
+	testVect3.setZ(3);
+	vector3 reeee = testVect3 * 2;
+	std::cout << reeee.ToString();
+	std::cout << std::endl;
+	vector3 r = testVect3 * 2.0;
+	std::cout << r.ToString();
+	std::cout << std::endl;
+	vector3 rer = testVect3 * 2.0f;
+	std::cout << rer.ToString();
+	std::cout << std::endl;
+	
+	vector3 erer = testVect3 ^ testVect31;
+	std::cout << erer.ToString() << std::endl;
+	std::cout << "Test for Matrix3" << std::endl;
 
 
 
